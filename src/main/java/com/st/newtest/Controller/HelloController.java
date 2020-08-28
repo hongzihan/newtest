b6254e8494d06a075e03c5f6c72636c5613ad2b2
ken <1601790617@qq.com>
2020-08-28 21:05:09 +0800

    增加权限管理页面

--- src/main/java/com/st/newtest/Controller/HelloController.java
+++ src/main/java/com/st/newtest/Controller/HelloController.java
@@ -3,6 +3,7 @@ package com.st.newtest.Controller;
 import com.st.newtest.Entity.User;
 import com.st.newtest.Util.CommonUtil;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,6 +29,10 @@ public class HelloController {
     public ModelAndView newForm1() {
         return CommonUtil.getPage("form-common");
     }
+
+    @RequiresRoles("supermanager")
+    @RequestMapping("/form-permission")
+    public ModelAndView formPermission() { return CommonUtil.getPage("form-validation");}
 
     @RequiresPermissions("charts")
     @RequestMapping("/charts")
