c532f6d997f56b3025272924bfbcc9c971ef1edd
ken <1601790617@qq.com>
2020-09-08 14:22:56 +0800

    1.权限显示图标更改为thymeleaf方法替代原js删除方法 2.引入最新版本font-awersome字体 3.解决css,js文件更改后不更新问题

--- src/main/java/com/st/newtest/config/WebMvcConfig.java
+++ src/main/java/com/st/newtest/config/WebMvcConfig.java
@@ -11,15 +11,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @version 1.0
  * @date 2018-11-16 14:57
  **/
-@Component
-public class WebMvcConfig implements WebMvcConfigurer {
-    /**
-     * 添加静态资源文件，外部可以直接访问地址
-     *
-     * @param registry
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-    }
-}
+//@Component
+//public class WebMvcConfig implements WebMvcConfigurer {
+//    /**
+//     * 添加静态资源文件，外部可以直接访问地址
+//     *
+//     * @param registry
+//     */
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+//    }
+//}
