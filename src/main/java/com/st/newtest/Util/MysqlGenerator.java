6a695ced1fbaaddc76ba3997d8c882e6b04fa2e2
ken <1601790617@qq.com>
2020-09-12 18:53:36 +0800

    引入mp的自动生成代码功能

---
+++ src/main/java/com/st/newtest/Util/MysqlGenerator.java
@@ -0,0 +1,66 @@
+package com.st.newtest.Util;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+public class MysqlGenerator {
+    public static void main(String[] args) {
+        AutoGenerator autoGenerator = new AutoGenerator();
+
+        //全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String oPath = System.getProperty("user.dir");//得到当前项目的路径
+        gc.setOutputDir(oPath + "/src/main/java");   //生成文件输出根目录
+        gc.setOpen(false);//生成完成后不弹出文件框
+        gc.setFileOverride(true);  //文件覆盖
+        gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(false);// XML columList
+        gc.setAuthor("ken");// 作者
+
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        gc.setControllerName("%sController");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        gc.setMapperName("%sMapper");
+        gc.setXmlName("%sMapper");
+        autoGenerator.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://120.78.216.226:3306/stmanage?useUnicode=true&serverTimezone=GMT&useSSL=false&characterEncoding=utf8");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("123456");
+        autoGenerator.setDataSource(dsc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);      // 表名生成策略
+        strategy.setInclude(new String[] { "chat_record" });     // 需要生成的表
+        strategy.setSuperServiceClass(null);
+        strategy.setSuperServiceImplClass(null);
+        strategy.setSuperMapperClass(null);
+        autoGenerator.setStrategy(strategy);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.st.newtest.stGame");
+        pc.setController("Controller");
+        pc.setService("Service");
+        pc.setServiceImpl("Service.impl");
+        pc.setMapper("Mapper");
+        pc.setEntity("Entity");
+        pc.setXml("xml");
+        autoGenerator.setPackageInfo(pc);
+
+        // 执行生成
+        autoGenerator.execute();
+    }
+
+}
